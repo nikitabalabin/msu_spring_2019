1b06814380ad53558e3c2d2c7843aa5e0c1e77c2
Nikita Balabin <nikita@mxl.ru>
2019-06-10 11:53:38 +0300

    hw5

---
+++ 05/hw5.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+
+std::mutex m;
+std::condition_variable cv;
+
+const int N = 1000000;
+int i = 0;
+
+void ping()
+{
+	std::unique_lock<std::mutex> lock(m);
+	while (i < N)
+	{
+		while (i % 2)
+			cv.wait(lock);
+
+		std::cout << "ping" << std::endl;
+
+		cv.notify_one();
+		i++;
+	}
+}
+
+
+void pong()
+{
+	std::unique_lock<std::mutex> lock(m);
+	while (i < N)
+	{
+		while (!(i % 2))
+			cv.wait(lock);
+
+		std::cout << "pong" << std::endl;
+
+		cv.notify_one();
+		i++;
+	}
+}
+
+int main()
+{
+	std::thread threadPing(ping), threadPong(pong);
+	threadPing.join();
+	threadPong.join();
+
+	return 0;
+}
